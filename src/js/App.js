0f0ebb135c94733a2a4fbd83e43fd52c5ac01bf1
remcovb <remcovanbogaert@icloud.com>
2018-06-10 16:42:06 +0200

    Navigation and QR code scanner that works

--- src/js/App.js
+++ src/js/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ClassComponent from './ClassComponent.jsx';
 
-const items = ["Amstel Gold Race", "Ronde van Spanje", "WK 1976", "WK 1981"];
-
 ReactDOM.render(
-  <ClassComponent items={items} />,
+  <ClassComponent />,
   document.getElementById('reactcontent')
 );

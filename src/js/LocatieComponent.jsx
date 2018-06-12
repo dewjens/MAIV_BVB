e50d6a0c3c7661119ebdece733865ca768a223c3
remcovb <remcovanbogaert@icloud.com>
2018-06-12 11:12:11 +0200

    mobile nav

--- src/js/LocatieComponent.jsx
+++ src/js/LocatieComponent.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-
 const LocatieComponent = () => {
   return (
     <div>
       <h2>Waar zijn onze billboards te vinden</h2>
-      <iframe src="https://snazzymaps.com/embed/77329" width="100%" height="600px"></iframe>
     </div>
   );
 }

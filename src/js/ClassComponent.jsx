95281c828bb3c85b826a4da30adc27b10577141b
remcovb <remcovanbogaert@icloud.com>
2018-06-14 15:32:11 +0200

    fixed my stupid ass push

---
+++ src/js/ClassComponent.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import HeaderComponent from './HeaderComponent.jsx';
+
+
+class ClassComponent extends Component {
+
+
+  render(){
+    return (
+      <HeaderComponent />
+    )
+  }
+
+}
+
+export default ClassComponent;
